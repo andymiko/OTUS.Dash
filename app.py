a13408b925338731679e1421da88ea40c2a0a67f
Andrey Kolesnik <kolesnik.andy@gmail.com>
2024-07-02 14:45:28 +0300

    lesson 2 фильтры без подключения данных

--- app.py
+++ app.py
@@ -7,17 +7,57 @@ import geopandas as gpd
 from graphfunc import print_bar_by_sales
 
 # ИНИЦИАЛИЗАЦИЯ ПРИЛОЖЕНИЯ И ЗАГРУЗКА ДАННЫХ
+app = Dash(__name__)
 
 df = pd.read_csv('data_for_otus.csv')
 
-app = Dash(__name__)
+# Дата начало-конец периода
+df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+min_date = df['OrderDate'].min()
+max_date = df['OrderDate'].max()
+
+# Начало-конец слайдера
+rent_max = df['rentabel'].max()
+rent_min = df['rentabel'].min()
+
+# Канал продаж
+sales_options = [{'value':col,'label':col} for col in df['Channel'].unique().tolist()]
 
 # ЭЛЕМЕНТЫ
+
+sales_channel = dcc.Dropdown(
+    id='sales_channel',
+    options=sales_options,
+    value=[],
+    clearable=False,
+    placeholder='Выберите канал продаж',
+    multi=True
+)
+
+rentable_slider = dcc.RangeSlider(
+    id='rent_slider',
+    min=rent_min,
+    max=rent_max,
+    value=[rent_min,rent_max],
+    dots=False,
+    tooltip={"placement": "bottom", "always_visible": True}
+)
+
+date_range = dcc.DatePickerRange(
+    id='data_filter',
+    display_format='DD-MM-YYYY',
+    min_date_allowed=min_date,
+    max_date_allowed=max_date,
+    start_date=min_date,
+    end_date=max_date)
 
 # ВЁРСТКА
 app.title = 'OTUS DASH'
 app.layout = html.Div([
     html.H1("HELLO WORLD!"),
+    html.Div(sales_channel),
+    html.Div(rentable_slider),
+    html.Div(date_range),
     dcc.Graph(figure=print_bar_by_sales(dataFrame=df))
 ])
 
--- graphfunc.py
+++ graphfunc.py
@@ -1,7 +1,8 @@
 import plotly.express as px
+
 def print_bar_by_sales(dataFrame):
     filtred_data = dataFrame.groupby(['Product Name']).agg({'Line Total': 'sum'}).reset_index()
     filtred_data.sort_values(by='Line Total', ignore_index=True, inplace=True)
     fig = px.bar(filtred_data, x="Product Name", y="Line Total", title="Продажи")
-    fig.update_layout(xaxis_title="Products", yaxis_title="Рубли")
+    fig.update_layout(xaxis_title="Продукт", yaxis_title="Рубли")
     return fig
